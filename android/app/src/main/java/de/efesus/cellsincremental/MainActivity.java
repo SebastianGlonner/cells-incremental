c0eb5d104628913719f49ed70a6e735d8dd22cde
Sebastian Glonner <efesus133@gmail.com>
2024-12-29 15:49:13 +0100

    add android with capacitor

---
+++ android/app/src/main/java/de/efesus/cellsincremental/MainActivity.java
@@ -0,0 +1,5 @@
+package de.efesus.cellsincremental;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.efesus.cellsincremental',
+  appName: 'CellsIncremental',
+  webDir: 'build',
+  bundledWebRuntime: true
+};
+
+export default config;
